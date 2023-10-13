97c6720bd36b1da1e1924d9e8b4d5f63111bd8fe
Kurt King <kurtaking@gmail.com>
2023-10-13 17:57:08 -0600

    test: filters and errors
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- plugins/announcements-backend/src/service/persistence/AnnouncementsDatabase.test.ts
+++ plugins/announcements-backend/src/service/persistence/AnnouncementsDatabase.test.ts
@@ -152,4 +152,176 @@ describe('AnnouncementsDatabase', () => {
       results: [],
     });
   });
+
+  it('handles not finding an announcement', async () => {
+    expect(await store.announcementByID('id')).toBeUndefined();
+  });
+
+  describe('filters', () => {
+    it('categories', async () => {
+      database = createDatabaseManager(testDbClient);
+      const categoryStore = (await initializePersistenceContext(database))
+        .categoriesStore;
+
+      await categoryStore.insert({
+        slug: 'category',
+        title: 'Category',
+      });
+
+      await categoryStore.insert({
+        slug: 'different',
+        title: 'A different category',
+      });
+
+      await store.insertAnnouncement({
+        id: 'id',
+        publisher: 'publisher',
+        title: 'title',
+        excerpt: 'excerpt',
+        body: 'body',
+        category: 'category',
+        created_at: DateTime.fromISO('2023-10-13T15:28:08.539'),
+      });
+
+      await store.insertAnnouncement({
+        id: 'id2',
+        publisher: 'publisher2',
+        title: 'title2',
+        excerpt: 'excerpt2',
+        body: 'body2',
+        category: 'category',
+        created_at: DateTime.fromISO('2023-10-13T15:28:08.539'),
+      });
+
+      await store.insertAnnouncement({
+        id: 'id3',
+        publisher: 'publisher3',
+        title: 'title3',
+        excerpt: 'excerpt3',
+        body: 'body3',
+        category: 'different',
+        created_at: DateTime.fromISO('2023-10-13T15:28:08.539'),
+      });
+
+      const announcements = await store.announcements({
+        category: 'category',
+      });
+
+      expect(announcements).toEqual({
+        count: 2,
+        results: [
+          {
+            id: 'id',
+            publisher: 'publisher',
+            title: 'title',
+            excerpt: 'excerpt',
+            body: 'body',
+            category: {
+              slug: 'category',
+              title: 'Category',
+            },
+            sticky: null,
+            type: null,
+            created_at: '2023-10-13 15:28:08.539 +00:00',
+          },
+          {
+            id: 'id2',
+            publisher: 'publisher2',
+            title: 'title2',
+            excerpt: 'excerpt2',
+            body: 'body2',
+            category: {
+              slug: 'category',
+              title: 'Category',
+            },
+            sticky: null,
+            type: null,
+            created_at: '2023-10-13 15:28:08.539 +00:00',
+          },
+        ],
+      });
+    });
+
+    it('offset', async () => {
+      await store.insertAnnouncement({
+        id: 'id',
+        publisher: 'publisher',
+        title: 'title',
+        excerpt: 'excerpt',
+        body: 'body',
+        created_at: DateTime.fromISO('2023-10-13T15:28:08.539'),
+      });
+
+      await store.insertAnnouncement({
+        id: 'id2',
+        publisher: 'publisher2',
+        title: 'title2',
+        excerpt: 'excerpt2',
+        body: 'body2',
+        created_at: DateTime.fromISO('2023-10-13T15:28:08.539'),
+      });
+
+      const announcements = await store.announcements({
+        offset: 1,
+      });
+
+      expect(announcements).toEqual({
+        count: 2,
+        results: [
+          {
+            id: 'id2',
+            publisher: 'publisher2',
+            title: 'title2',
+            excerpt: 'excerpt2',
+            body: 'body2',
+            category: undefined,
+            sticky: null,
+            type: null,
+            created_at: '2023-10-13 15:28:08.539 +00:00',
+          },
+        ],
+      });
+    });
+
+    it('max', async () => {
+      await store.insertAnnouncement({
+        id: 'id',
+        publisher: 'publisher',
+        title: 'title',
+        excerpt: 'excerpt',
+        body: 'body',
+        created_at: DateTime.fromISO('2023-10-13T15:28:08.539'),
+      });
+
+      await store.insertAnnouncement({
+        id: 'id2',
+        publisher: 'publisher2',
+        title: 'title2',
+        excerpt: 'excerpt2',
+        body: 'body2',
+        created_at: DateTime.fromISO('2023-10-13T15:28:08.539'),
+      });
+
+      const announcements = await store.announcements({
+        max: 1,
+      });
+
+      expect(announcements).toEqual({
+        count: 2,
+        results: [
+          {
+            id: 'id',
+            publisher: 'publisher',
+            title: 'title',
+            excerpt: 'excerpt',
+            body: 'body',
+            category: undefined,
+            sticky: null,
+            type: null,
+            created_at: '2023-10-13 15:28:08.539 +00:00',
+          },
+        ],
+      });
+    });
+  });
 });

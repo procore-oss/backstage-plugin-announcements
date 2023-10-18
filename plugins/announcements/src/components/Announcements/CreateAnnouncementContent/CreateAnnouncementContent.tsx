b21b6bd68ab1a752f69b5a0748a331594bcff51f
Kurt King <kurtaking@gmail.com>
2023-10-18 10:38:03 -0600

    refactor: revert to more similar state as original
    This refactor embeds the announcement form in content cards to give
    a better UI experience.
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- plugins/announcements/src/components/Announcements/CreateAnnouncementContent/CreateAnnouncementContent.tsx
+++ plugins/announcements/src/components/Announcements/CreateAnnouncementContent/CreateAnnouncementContent.tsx
@@ -1,38 +1,63 @@
-import React from 'react';
-import { Content } from '@backstage/core-components';
+import React, { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Page, Header, Content } from '@backstage/core-components';
 import {
   alertApiRef,
   identityApiRef,
   useApi,
+  useRouteRef,
 } from '@backstage/core-plugin-api';
-import { announcementsApiRef, CreateAnnouncementRequest } from '../../../api';
-import { Announcement } from '@procore-oss/backstage-plugin-announcements-common';
-import { SubmitHandler } from 'react-hook-form';
-import {
-  AnnouncementForm,
-  AnnouncementFormInputs,
-} from '../AnnouncementForm/AnnouncementForm';
 
-export const CreateAnnouncementContent = () => {
+import { AnnouncementForm } from '../AnnouncementForm';
+import { CreateAnnouncementRequest, announcementsApiRef } from '../../../api';
+import { Announcement } from '@procore-oss/backstage-plugin-announcements-common';
+import { rootRouteRef } from '../../../routes';
+import { SubmitHandler } from 'react-hook-form';
+import { AnnouncementFormInputs } from '../AnnouncementForm/AnnouncementForm';
+
+type CreateAnnouncementPageProps = {
+  themeId: string;
+  title: string;
+  subtitle?: ReactNode;
+};
+
+export const CreateAnnouncementContent = (
+  props: CreateAnnouncementPageProps,
+) => {
   const announcementsApi = useApi(announcementsApiRef);
-  const identityApi = useApi(identityApiRef);
+  const rootPage = useRouteRef(rootRouteRef);
   const alertApi = useApi(alertApiRef);
+  const navigate = useNavigate();
+  const identityApi = useApi(identityApiRef);
 
   const onSubmit: SubmitHandler<AnnouncementFormInputs> = async formData => {
     const { userEntityRef } = await identityApi.getBackstageIdentity();
 
-    const newAnnouncement: CreateAnnouncementRequest = {
-      ...formData,
-      category: formData.category?.slug,
-      publisher: userEntityRef,
-    };
-    await announcementsApi.createAnnouncement(newAnnouncement);
-    alertApi.post({ message: 'Announcement created', severity: 'success' });
+    try {
+      const newAnnouncement: CreateAnnouncementRequest = {
+        ...formData,
+        category: formData.category?.slug,
+        publisher: userEntityRef,
+      };
+
+      await announcementsApi.createAnnouncement(newAnnouncement);
+      alertApi.post({ message: 'Announcement created', severity: 'success' });
+      navigate(rootPage());
+    } catch (err) {
+      alertApi.post({ message: (err as Error).message, severity: 'error' });
+    }
   };
 
   return (
-    <Content>
-      <AnnouncementForm announcement={{} as Announcement} onSubmit={onSubmit} />
-    </Content>
+    <Page themeId={props.themeId}>
+      <Header title={props.title} subtitle={props.subtitle} />
+
+      <Content>
+        <AnnouncementForm
+          announcement={{} as Announcement}
+          onSubmit={onSubmit}
+        />
+      </Content>
+    </Page>
   );
 };
--- plugins/announcements/src/components/Announcements/EditAnnouncementContent/EditAnnouncementContent.tsx
+++ plugins/announcements/src/components/Announcements/EditAnnouncementContent/EditAnnouncementContent.tsx
@@ -1,54 +1,58 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { useAsync } from 'react-use';
-import { Content, Progress } from '@backstage/core-components';
+import { Page, Header, Content, Progress } from '@backstage/core-components';
 import {
   alertApiRef,
-  identityApiRef,
   useApi,
   useRouteRefParams,
 } from '@backstage/core-plugin-api';
 import { Alert } from '@material-ui/lab';
-import { announcementEditRouteRef } from '../../../routes';
-import { announcementsApiRef, CreateAnnouncementRequest } from '../../../api';
 import { AnnouncementForm } from '../AnnouncementForm';
+import { announcementEditRouteRef } from '../../../routes';
+import { announcementsApiRef } from '../../../api';
 import { SubmitHandler } from 'react-hook-form';
 import { AnnouncementFormInputs } from '../AnnouncementForm/AnnouncementForm';
 
-export const EditAnnouncementContent = () => {
+type EditAnnouncementPageProps = {
+  themeId: string;
+  title: string;
+  subtitle?: ReactNode;
+};
+
+export const EditAnnouncementContent = (props: EditAnnouncementPageProps) => {
   const announcementsApi = useApi(announcementsApiRef);
-  const identityApi = useApi(identityApiRef);
   const alertApi = useApi(alertApiRef);
   const { id } = useRouteRefParams(announcementEditRouteRef);
   const { value, loading, error } = useAsync(async () =>
     announcementsApi.announcementByID(id),
   );
 
-  const onSubmit: SubmitHandler<AnnouncementFormInputs> = async formData => {
-    const { userEntityRef } = await identityApi.getBackstageIdentity();
+  let title = props.title;
+  let content: React.ReactNode = <Progress />;
 
-    const updatedAnnouncement: CreateAnnouncementRequest = {
-      ...formData,
-      category: formData.category?.slug,
-      publisher: userEntityRef,
-    };
-
+  const onSubmit: SubmitHandler<AnnouncementFormInputs> = async request => {
     try {
-      await announcementsApi.updateAnnouncement(id, updatedAnnouncement);
+      await announcementsApi.updateAnnouncement(id, request);
       alertApi.post({ message: 'Announcement updated.', severity: 'success' });
     } catch (err) {
       alertApi.post({ message: (err as Error).message, severity: 'error' });
     }
   };
 
-  let content;
-
   if (loading) {
     content = <Progress />;
   } else if (error) {
     content = <Alert severity="error">{error.message}</Alert>;
   } else {
+    title = `Edit "${value.title}" – ${title}`;
     content = <AnnouncementForm announcement={value} onSubmit={onSubmit} />;
   }
 
-  return <Content>{content}</Content>;
+  return (
+    <Page themeId={props.themeId}>
+      <Header title={title} subtitle={props.subtitle} />
+
+      <Content>{content}</Content>
+    </Page>
+  );
 };

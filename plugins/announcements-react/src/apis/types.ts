1a7b0eb70c51371f5c5963e99571194e26703934
Kurt King <kurtaking@gmail.com>
2024-02-03 15:55:02 -0700

    feat: migrate AnnouncementsApi (#215)
    Signed-off-by: Kurt King <kurtaking@gmail.com>

---
+++ plugins/announcements-react/src/apis/types.ts
@@ -0,0 +1,30 @@
+export type Category = {
+  slug: string;
+  title: string;
+};
+
+export type Announcement = {
+  id: string;
+  category?: Category;
+  publisher: string;
+  title: string;
+  excerpt: string;
+  body: string;
+  created_at: string;
+};
+
+export type AnnouncementsList = {
+  count: number;
+  results: Announcement[];
+};
+
+export type CreateAnnouncementRequest = Omit<
+  Announcement,
+  'id' | 'category' | 'created_at'
+> & {
+  category?: string;
+};
+
+export type CreateCategoryRequest = {
+  title: string;
+};

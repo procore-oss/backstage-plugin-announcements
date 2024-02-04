e5c068589873857524bbee4b483faec945001258
Kurt King <kurtaking@gmail.com>
2024-02-04 00:46:16 -0700

    feat: default announcements service in `announcements-node` plugin (#224)
    * feat: default announcements service
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    * refactor: use client from announcements-node
    
    to remove dependency on announcements-backend.
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>
    
    ---------
    
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- plugins/search-backend-module-announcements/src/collators/AnnouncementCollatorFactory.ts
+++ plugins/search-backend-module-announcements/src/collators/AnnouncementCollatorFactory.ts
@@ -5,7 +5,7 @@ import {
   DocumentCollatorFactory,
   IndexableDocument,
 } from '@backstage/plugin-search-common';
-import { AnnouncementsClient } from '@procore-oss/backstage-plugin-announcements-backend';
+import { DefaultAnnouncementsService } from '@procore-oss/backstage-plugin-announcements-node';
 import { Announcement } from '@procore-oss/backstage-plugin-announcements-common';
 
 type IndexableAnnouncementDocument = IndexableDocument & {
@@ -22,7 +22,7 @@ export class AnnouncementCollatorFactory implements DocumentCollatorFactory {
   public readonly type: string = 'announcements';
 
   private logger: Logger;
-  private announcementsClient: AnnouncementsClient;
+  private announcementsClient: DefaultAnnouncementsService;
 
   static fromConfig(options: AnnouncementCollatorOptions) {
     return new AnnouncementCollatorFactory(options);
@@ -30,7 +30,7 @@ export class AnnouncementCollatorFactory implements DocumentCollatorFactory {
 
   private constructor(options: AnnouncementCollatorOptions) {
     this.logger = options.logger;
-    this.announcementsClient = new AnnouncementsClient({
+    this.announcementsClient = new DefaultAnnouncementsService({
       discoveryApi: options.discoveryApi,
     });
   }

58a0238626343ccb0a5fb18d506f8fab89de8a6e
Kurt King <kurtaking@gmail.com>
2024-02-03 23:54:34 -0700

    feat: default announcements service
    Signed-off-by: Kurt King <kurtaking@gmail.com>

---
+++ plugins/announcements-node/src/DefaultAnnouncementsService.ts
@@ -0,0 +1,39 @@
+import {
+  Announcement,
+  AnnouncementsList,
+} from '@procore-oss/backstage-plugin-announcements-common';
+import { AnnouncementsService } from './AnnouncementsService';
+import { DiscoveryApi } from '@backstage/core-plugin-api';
+import { ResponseError } from '@backstage/errors';
+
+type AnnouncementsServiceOptions = {
+  discoveryApi: DiscoveryApi;
+};
+
+export class DefaultAnnouncementsService implements AnnouncementsService {
+  private readonly discoveryApi: DiscoveryApi;
+
+  constructor(opts: AnnouncementsServiceOptions) {
+    this.discoveryApi = opts.discoveryApi;
+  }
+
+  static create(opts: AnnouncementsServiceOptions) {
+    return new DefaultAnnouncementsService(opts);
+  }
+
+  private async fetch<T = any>(input: string): Promise<T> {
+    const baseApiUrl = await this.discoveryApi.getBaseUrl('announcements');
+
+    return fetch(`${baseApiUrl}${input}`).then(async response => {
+      if (!response.ok) {
+        throw await ResponseError.fromResponse(response);
+      }
+      return response.json() as Promise<T>;
+    });
+  }
+
+  async announcements(): Promise<Announcement[]> {
+    const result = await this.fetch<AnnouncementsList>('/announcements');
+    return result?.results;
+  }
+}

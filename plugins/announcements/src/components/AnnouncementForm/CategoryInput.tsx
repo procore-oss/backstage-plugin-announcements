debc73e21306b0a9ae8f82d01875862b820eda8e
Kurt King <kurtaking@gmail.com>
2024-05-09 15:03:52 -0600

    feat: dynamically create category
    Signed-off-by: Kurt King <kurtaking@gmail.com>

---
+++ plugins/announcements/src/components/AnnouncementForm/CategoryInput.tsx
@@ -0,0 +1,121 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
+import { Category } from '@procore-oss/backstage-plugin-announcements-common';
+import { useApi } from '@backstage/core-plugin-api';
+import { announcementsApiRef } from '@procore-oss/backstage-plugin-announcements-react';
+import { useAsync } from 'react-use';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+type CategoryInputProps = {
+  setForm: (
+    value: React.SetStateAction<{
+      category: string | undefined;
+      id: string;
+      publisher: string;
+      title: string;
+      excerpt: string;
+      body: string;
+      created_at: string;
+    }>,
+  ) => void;
+  form: {
+    category: string | undefined;
+    id: string;
+    publisher: string;
+    title: string;
+    excerpt: string;
+    body: string;
+    created_at: string;
+  };
+  initialValue: string;
+};
+
+const filter = createFilterOptions<Category>();
+
+export default function CategoryInput({
+  setForm,
+  form,
+  initialValue,
+}: CategoryInputProps) {
+  const announcementsApi = useApi(announcementsApiRef);
+
+  const { value: categories, loading: categoriesLoading } = useAsync(
+    () => announcementsApi.categories(),
+    [announcementsApi],
+  );
+
+  return (
+    <Autocomplete
+      fullWidth
+      value={initialValue ?? ''}
+      onChange={async (_, newValue) => {
+        if (!newValue) {
+          setForm({ ...form, category: undefined });
+          return;
+        }
+
+        const newCategory = (
+          typeof newValue === 'string' ? newValue : newValue.title
+        )
+          .replace('Create "', '')
+          .replace('"', '');
+
+        setForm({ ...form, category: newCategory });
+      }}
+      filterOptions={(options, params) => {
+        const filtered = filter(options, params);
+        const { inputValue } = params;
+
+        /*
+          Suggest the creation of a new category. This adds the new value to the list of options
+          and creates the new category when the form is submitted.
+        */
+        const isExisting = options.some(option => inputValue === option.title);
+        if (inputValue !== '' && !isExisting) {
+          filtered.push({
+            title: `Create "${inputValue}"`,
+            slug: inputValue.toLowerCase(),
+          });
+        }
+
+        return filtered;
+      }}
+      selectOnFocus
+      handleHomeEndKeys
+      loading={categoriesLoading}
+      id="category-input-field"
+      options={categories || []}
+      getOptionLabel={option => {
+        // Value selected with enter, right from the input
+        if (typeof option === 'string') {
+          return option;
+        }
+
+        return option.slug;
+      }}
+      renderOption={(props, option) => <li {...props}>{option.title}</li>}
+      freeSolo
+      renderInput={params => (
+        <TextField
+          {...params}
+          id="category"
+          label="Category"
+          variant="outlined"
+          fullWidth
+          InputProps={{
+            ...params.InputProps,
+            endAdornment: (
+              <>
+                {categoriesLoading ? (
+                  <CircularProgress color="inherit" size={20} />
+                ) : null}
+                {params.InputProps.endAdornment}
+              </>
+            ),
+          }}
+        />
+      )}
+    />
+  );
+}

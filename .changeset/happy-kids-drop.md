7158f7676860e27f04a34b64f5633c5f8b451030
Alex McKay <77302540+alex-mckay@users.noreply.github.com>
2024-08-27 11:45:41 -0400

    add default entity filter & use 1.30 FE syntax (#409)
    * upgrade to 1.30 utilities & add default entity filter
    
    Signed-off-by: Alex McKay <amckay@spotify.com>
    
    * update changelog
    
    Signed-off-by: Alex McKay <amckay@spotify.com>
    
    * update docs
    
    Signed-off-by: Alex McKay <amckay@spotify.com>
    
    ---------
    
    Signed-off-by: Alex McKay <amckay@spotify.com>

---
+++ .changeset/happy-kids-drop.md
@@ -0,0 +1,5 @@
+---
+'@procore-oss/backstage-plugin-announcements': patch
+---
+
+Make entity card appear on components & systems only by default in new FE system
--- plugins/announcements/src/alpha/navItems.tsx
+++ plugins/announcements/src/alpha/navItems.tsx
@@ -1,4 +1,4 @@
-import { createNavItemExtension } from '@backstage/frontend-plugin-api';
+import { NavItemBlueprint } from '@backstage/frontend-plugin-api';
 import { convertLegacyRouteRef } from '@backstage/core-compat-api';
 import NotificationsIcon from '@material-ui/icons/Notifications';
 import { rootRouteRef } from '../routes';
@@ -6,10 +6,12 @@ import { rootRouteRef } from '../routes';
 /**
  * @alpha
  */
-export const announcementsNavItem = createNavItemExtension({
-  title: 'Announcements',
-  routeRef: convertLegacyRouteRef(rootRouteRef),
-  icon: NotificationsIcon,
+export const announcementsNavItem = NavItemBlueprint.make({
+  params: {
+    title: 'Announcements',
+    routeRef: convertLegacyRouteRef(rootRouteRef),
+    icon: NotificationsIcon,
+  },
 });
 
 export default [announcementsNavItem];

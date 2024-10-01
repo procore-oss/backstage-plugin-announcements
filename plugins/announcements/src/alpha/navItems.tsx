9d9420dd3212cf57b9c3f85c8fdf5aece78b9696
Kurt King <kurtaking@gmail.com>
2024-09-30 22:44:22 -0600

    fix: discrepencies
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- plugins/announcements/src/alpha/navItems.tsx
+++ plugins/announcements/src/alpha/navItems.tsx
@@ -1,7 +1,10 @@
 import { NavItemBlueprint } from '@backstage/frontend-plugin-api';
 import { convertLegacyRouteRef } from '@backstage/core-compat-api';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import { rootRouteRef } from '../routes';
+
+// todo: replace once `NavItemBlueprint.make()` supports mui v5
+import NotificationsIcon from '@material-ui/icons/Notifications';
+// import NotificationsIcon from '@mui/icons-material/Notifications';
 
 /**
  * @alpha

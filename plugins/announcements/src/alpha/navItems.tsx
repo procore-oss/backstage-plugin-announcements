af429d84c88aff55d1fe235eec86b6168d5bea57
Kurt King <kurtaking@gmail.com>
2024-10-02 11:15:13 -0600

    must support material ui icon in NavItemBlueprint
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- plugins/announcements/src/alpha/navItems.tsx
+++ plugins/announcements/src/alpha/navItems.tsx
@@ -2,9 +2,14 @@ import { NavItemBlueprint } from '@backstage/frontend-plugin-api';
 import { convertLegacyRouteRef } from '@backstage/core-compat-api';
 import { rootRouteRef } from '../routes';
 
-// todo: replace once `NavItemBlueprint.make()` supports mui v5
-import NotificationsIcon from '@material-ui/icons/Notifications';
+/*
+
+TODO: replace once `NavItemBlueprint.make()` supports mui v5
+      remove @material-ui dep in package.json
+
+*/
 // import NotificationsIcon from '@mui/icons-material/Notifications';
+import NotificationsIcon from '@material-ui/icons/Notifications';
 
 /**
  * @alpha

98cfc55295405e4804e3f910069591628d0d39bd
Kurt King <kurtaking@gmail.com>
2024-12-06 10:45:43 -0700

    bump backstage to 1.33.5
    Signed-off-by: Kurt King <kurtaking@gmail.com>

--- plugins/announcements-backend/dev/index.ts
+++ plugins/announcements-backend/dev/index.ts
@@ -1,13 +1,13 @@
 import { createBackend } from '@backstage/backend-defaults';
 
 const backend = createBackend();
-backend.add(import('@backstage/plugin-permission-backend/alpha'));
+backend.add(import('@backstage/plugin-permission-backend'));
 backend.add(
   import('@backstage/plugin-permission-backend-module-allow-all-policy'),
 );
 backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-guest-provider'));
-backend.add(import('@backstage/plugin-events-backend/alpha'));
+backend.add(import('@backstage/plugin-events-backend'));
 backend.add(import('@backstage/plugin-signals-backend'));
 backend.add(import('../src'));
 
